77d72e7930a05334a7e37c86697cca1ae6fd653e
Kavya Muppalla <kavyamuppalla@zipcoders-MBP-2.fios-router.home>
2019-06-08 22:49:41 -0400

    Item Sorter

--- src/main/java/rocks/zipcode/io/Item.java
+++ src/main/java/rocks/zipcode/io/Item.java
@@ -4,30 +4,56 @@ package rocks.zipcode.io;
  * @author leon on 30/01/2019.
  */
 public class Item {
+    private Long id;
+    private String name;
+    private Double price;
     public Item() {
+        id = 0L;
+        name = "";
+        price = 0.01;
     }
 
     public Item(Long id, String name, Double price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+
     }
 
     public String getName() {
-        return null;
+
+        return this.name;
     }
 
     public void setName(String name) {
+        this.name = name;
     }
 
     public Long getId() {
-        return null;
+
+        return this.id;
     }
 
     public void setId(Long id) {
+        this.id = id;
     }
 
     public Double getPrice() {
-        return null;
+
+        return this.price;
     }
 
     public void setPrice(Double price) {
+        this.price = price;
     }
+
+    /*public int compareTo(Item o2) {
+        if(id > o2.id) {
+            return 1;
+        }
+        else if(id < o2.id) {
+            return -1;
+        }
+        return 0;
+    }*/
 }
--- src/main/java/rocks/zipcode/io/ItemSorter.java
+++ src/main/java/rocks/zipcode/io/ItemSorter.java
@@ -1,15 +1,27 @@
 package rocks.zipcode.io;
 
+import java.util.Arrays;
 import java.util.Comparator;
 
 /**
  * @author leon on 30/01/2019.
  */
 public class ItemSorter {
+    private Item[] items;
     public ItemSorter(Item[] items) {
+        this.items = items;
+    }
+
+    public Item[] getItems() {
+        return items;
+    }
+
+    public void setItems(Item[] items) {
+        this.items = items;
     }
 
     public Item[] sort(Comparator<Item> comparator) {
-        return null;
+        Arrays.sort(items,comparator);
+        return items;
     }
 }
